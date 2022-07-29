90a713513f64fbbab88e8453e021373bf23c5252
junb119 <emfowkd@gmail.com>
2022-07-29 11:29:16 +0900

    비교 연산자

--- test/main.js
+++ test/main.js
@@ -1,7 +1,13 @@
-// 할당 연산자
+// 비교 연산자(comparison operator)
+console.log('비교 연산자')
+let b = 1
+let c = 2
+console.log( b === c)
 
-console.log('할당 연산자')
-let a = 2
-console.log('a: ',a)
-a += 1
-console.log('a: ',a)
+function isEqual(x,y) {
+  return x === y
+}
+console.log(isEqual(1,1))
+console.log(isEqual(1,2))
+
+console.log( b >= c)
