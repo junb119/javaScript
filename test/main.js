22f520d5dbe25dca2544d5390b3aa2213387ee04
junb119 <emfowkd@gmail.com>
2022-07-29 13:15:10 +0900

    타이머 함수

--- test/main.js
+++ test/main.js
@@ -1,16 +1,27 @@
-// 호이스팅(Hoisting)
-// 함수 선언부가 유효범위 최상단으로 끌어올려지는 현상
+//  타이머 함수
+// setTimout(함수, 시간) : 일정 시간 후 함수 실행
+// setInterval(함수, 시간) : 시간 간격마다 함수 실행
+// clearTimerout() : 설정된 Timeout 함수를 종료
+// clearInterval() : 설정된 Interval 함수를 종료
 
-// ver1 
-const a = 7
+// setTimout(함수, 시간) : 일정 시간 후 함수 실행
+const timer = setTimeout(() => {
+  console.log('Heropy')
+} , 3000 ) // 3초뒤 'Heropy' 출력
 
-double()  
-const double =  function () {  // 함수 표현의 경우 함수 호출이 먼저 있으면 실행 되지 않음
-  console.log(a*2)
-};
+// clearTimerout() : 설정된 Timeout 함수를 종료
+const h1El = document.querySelector('h1')
+h1El.addEventListener('click', () => {
+  clearTimeout(timer)
+}) // 'h1'을 클릭하면 setTimeout 함수 종료
 
-// ver 2
-double2()
-function double2() {   // 함수 선언인 경우 함소 호출이 먼저 있어도 실행가능
-  console.log(a*2)
-}
+// setInterval(함수, 시간) : 시간 간격마다 함수 실행
+const timer2 = setInterval(() => {
+  console.log('Heropy')
+} , 3000 ) // 3초마다 한번씩 'Heropy' 출력
+
+// 'h1'을 클릭하면 timer 실행x
+const h1El2 = document.querySelector('h1')
+h1El2.addEventListener('click', () => {
+  clearInterval(timer2)
+})
