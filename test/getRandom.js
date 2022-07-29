2db859cf915c22fa75c523f261b2c9caf232fdd1
junb119 <emfowkd@gmail.com>
2022-07-29 12:20:46 +0900

    조건문 if else

---
+++ test/getRandom.js
@@ -0,0 +1,7 @@
+export default function random() {
+  return Math.floor(Math.random() * 10)
+}
+
+
+// Math.random() : 랜덤
+// Math.floor(3.14) : 정수
--- test/main.js
+++ test/main.js
@@ -1,5 +1,15 @@
-// 삼항 연산자
+import abc from './getRandom'
 
-const a = 1
-console.log(a ? '참' : '거짓')
-// 참이면 ':' 의 앞쪽, 거짓이면 ':'의 뒷쪽
+// 조건문 (If statement)
+
+const a = random()
+
+if (a === 0 ) {
+  console.log('a is 0')
+} else if (a === 2) {
+  console.log('a is 2')
+} else if (a === 4) {
+  console.log('a is 4')
+} else {
+  console.log('rest...')
+}
