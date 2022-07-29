099ced0505eac002a328b7b2c34709ef9a0a7608
junb119 <emfowkd@gmail.com>
2022-07-29 12:44:48 +0900

    함수 복습 arguments

--- test/main.js
+++ test/main.js
@@ -1,11 +1,10 @@
-// 함수 복습
-function sum(x,y) {
-  return x + y
-}
-a = sum(1,3)
-b = sum(4,12)
-console.log(a)
-console.log(b)
+// arguments
+// arguments 객체는 매개변수를 지정하지 않아도 함수 안에서 언제든지 사용 가능
 
-// 함수를 여러번 실행시키기보다 
-// 함수를 실행한 결과를 변수에 넣어 재활용하는게 메모리값을 아낄 수 있다.
+
+function sum () {
+  console.log(arguments)
+  return arguments[0] + arguments[1]
+}
+
+console.log(sum(7,3))
