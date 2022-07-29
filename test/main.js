92059e5b62567411ffb35c5eedf256ab3c85b26f
junb119 <emfowkd@gmail.com>
2022-07-29 13:19:15 +0900

    콜백

--- test/main.js
+++ test/main.js
@@ -1,27 +1,16 @@
-//  타이머 함수
-// setTimout(함수, 시간) : 일정 시간 후 함수 실행
-// setInterval(함수, 시간) : 시간 간격마다 함수 실행
-// clearTimerout() : 설정된 Timeout 함수를 종료
-// clearInterval() : 설정된 Interval 함수를 종료
+// 콜백(Callback)
+// 함수의 인수로 사용되는 함수
 
-// setTimout(함수, 시간) : 일정 시간 후 함수 실행
-const timer = setTimeout(() => {
-  console.log('Heropy')
-} , 3000 ) // 3초뒤 'Heropy' 출력
+// setTimeout(함수, 시간)
 
-// clearTimerout() : 설정된 Timeout 함수를 종료
-const h1El = document.querySelector('h1')
-h1El.addEventListener('click', () => {
-  clearTimeout(timer)
-}) // 'h1'을 클릭하면 setTimeout 함수 종료
+function timeout(cb) {
+  setTimeout(() => {
+    console.log('Heropy!')
+    cb()
+  }, 3000)
+}
+timeout(() => {  // 콜백함수 (timeout함수의 매개변수 cd로 들어감)
+  console.log('Done!')
+} )  
 
-// setInterval(함수, 시간) : 시간 간격마다 함수 실행
-const timer2 = setInterval(() => {
-  console.log('Heropy')
-} , 3000 ) // 3초마다 한번씩 'Heropy' 출력
 
-// 'h1'을 클릭하면 timer 실행x
-const h1El2 = document.querySelector('h1')
-h1El2.addEventListener('click', () => {
-  clearInterval(timer2)
-})
