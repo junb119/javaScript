177e04fc7452c0238514eb08eacbb8429e3c70b6
junb119 <emfowkd@gmail.com>
2022-07-29 11:27:11 +0900

    할당 연산자

--- test/main.js
+++ test/main.js
@@ -1,8 +1,7 @@
-/// 산술연산자
+// 할당 연산자
 
-console.log('산술 연산자')
-console.log(1 + 2)
-console.log(5 - 7)
-console.log(3 * 4)
-console.log(10 / 2)
-console.log(10 % 2)
+console.log('할당 연산자')
+let a = 2
+console.log('a: ',a)
+a += 1
+console.log('a: ',a)
