84a3478317fd6ec88719a553ab061c3bc1d396ca
junb119 <emfowkd@gmail.com>
2022-07-29 10:48:15 +0900

    데이터 타입 확인

---
+++ test/gettype.js
@@ -0,0 +1,5 @@
+// 내보내기
+
+export default function getType(data) {
+  return Object.prototype.toString.call(data).slice(8,-1)
+}
--- test/main.js
+++ test/main.js
@@ -1,4 +1,13 @@
-import _ from 'lodash'
+import getType from './getType'
 
-console.log('hello')
-console.log(_.camelCase('hello world'))
+console.log(typeof 'hello world')
+console.log(typeof 123)
+console.log(typeof true)
+console.log(typeof undefined)
+console.log(typeof null)
+console.log(typeof {} )
+console.log(typeof [] )
+
+console.log(getType(123))
+console.log(getType(false))
+console.log(getType('????'))
