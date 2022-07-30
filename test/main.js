94ce75bfe446d343bfd590c3d6ec484de6563fdc
junb119 <emfowkd@gmail.com>
2022-07-30 18:27:14 +0900

    ES6 Classes1

--- test/main.js
+++ test/main.js
@@ -1,36 +1,12 @@
-// this
-// 일반(Normal) 함수는 호출 위치에 따라 this 정의!
-// 화살표(Arrow) 함수는 자신이 선언된 함수 범위에서 this 정의
-
-// ver1
+// ES6 Classes
 const heropy = {
-  name : 'Heropy',
-  normal : function (){
-    console.log(this.name) // name : 'Heropy'
+  name: 'Heropy2',
+  normal() { // = normal : function() {}
+    console.log(this.name)
   },
   arrow : () => {
-        console.log(this.name) // name : undefined
+    console.log(this.name)
   }
 }
 heropy.normal()
 heropy.arrow()
-
-// ver2
-const amy = {
-  name : 'Amy',
-  normal : heropy.normal, // 'Heropy'
-  arrow: heropy.arrow // undifined
-}
-amy.normal()
-amy.arrow()
-
-// ver3
-let time = {
-  name : 'Heropy!!!!',
-  timeout : function() {
-    setTimeout(() => {
-      console.log(this.name)
-    }, 2000)
-  }
-}
-time.timeout()
