02910951f500a68649e258eac090523babbc4917
junb119 <emfowkd@gmail.com>
2022-08-08 11:53:24 +0900

    배열2

--- test/main.js
+++ test/main.js
@@ -1,11 +1,38 @@
+// .includes() 메소드 : 인수로 사용된 데이터 포함 여부 반환
+
 let array1 = [1, 2, 3, 4, 5]
 let array2 = ['apple', 'banana', 'cherry']
 
-// .find() .findIndex() : 해당되는 값 반환
+const a = array1.includes(3) // 포함 >>> true
+console.log(a)
+const b = array1.includes('vvd') // 포함X false
+console.log(b)
 
-let a = array2.find(el => /^c/.test(el) // /^B/ : 대문자 B로 시작하는지
-)                                        // 조건에 맞는 요소 반환
-console.log(a) 
-a = array2.findIndex(el => /^c/.test(el) // 조건에 맞는 인덱스 반환
-)
-console.log(a) 
+// .push() : 배열 맨 뒤에 인수로 사용된 데이터 삽입
+// .unshift() : 배열 맨 앞에 인수로 사용된 데이터 삽입
+// 원본 수정주의
+
+array1.push(5)  
+console.log(array1)
+
+array1.unshift(0)  // 
+console.log(array1)
+
+// .reverse() : 배열의 순서뒤집기
+// 원본 수정주의
+
+array1.reverse()
+array2.reverse()
+
+console.log(array1)
+console.log(array2)
+
+// .splice() : (해당 인덱스, 삭제할 개수, 삽입할 데이터)
+// 원본 수정 주의
+
+array1.splice(2,1) // 2, 1: index, 1개  : 해당 인덱스번호부터 1개 삭제
+console.log(array1)
+array1.splice(2,0,999) // 2,0,999 : 인덱스 2에서 하나도 지우지말고 999를 해당 인덱스에 끼워놓음
+console.log(array1)
+array1.splice(2,1,66) // 2,0,66 : 인덱스 2에서 1개를 지우고 66을 해당 인덱스에 끼워놓음
+console.log(array1)
