142e96250d4aafa9a74318975dc1dc93ad3a8fb8
junb119 <emfowkd@gmail.com>
2022-08-08 11:29:50 +0900

    배열

--- test/main.js
+++ test/main.js
@@ -1,22 +1,30 @@
-// toFixed : 소수점 기준 자르기, String으로 반환
+// 배열
+let array1 = [5, 12, 0, 130, 44]
+let array2 = ['apple', 'banana', 'cherry']
 
-const pi = 3.14238927839123
-console.log(pi)
+// find 함수 : 배열을 돌며 콜백 함수의 조건에 부합하는 첫번째 아이템 값을 반환
+console.log(array1.find(element => element > 10))  // false, true false true true => 12
 
-const str = pi.toFixed(2)
-console.log(str)
-console.log(typeof str)
+// length 속성 : 배열의 item 갯수 반환
+console.log('item 갯수 :', array1.length) 
 
+// concat 함수 : 배열을 병합하는 메소드, 원본데이터는 그대로
+console.log('array1 + array2 :' , array1.concat(array2)) 
 
-// parseInt, parseFloat : 형변환
+// .forEach 함수
+// forEach 함수의 매개변수는 함수 사용(=콜백)
+// 콜백 함수는 (요소, 인덱스, 배열)의 매개변수를 가짐
+array2.forEach(function (element, index, array) {       
+  console.log(element, index, array) })
 
-const integer = parseInt(str)
-const float = parseFloat(str)
-console.log(integer)
-console.log(float) 
-console.log(typeof integer, typeof float)
+// .map() 함수 : 콜백으로 배열의 요소와 인덱스를 새로운 배열로 '반환'(=return 사용)
+let b = array2.map((element, index) => ({   
+  id : index,   
+  name: element 
+}))
+console.log(b)
 
-const ll = 3.14238927839123
-const dd = parseInt(pi.toFixed(2))
-console.log(dd)
-
+// .filter() : 배열의 요소 중 콜백으로 정의된 조건에 맞는 값만 배열로 반환
+b = array1.filter(number => number < 3)
+console.log(b)
+console.log(array1)
