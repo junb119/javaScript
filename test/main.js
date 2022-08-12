265defe070b3359001ad910feff32827d925f661
junb119 <emfowkd@gmail.com>
2022-08-12 22:04:11 +0900

    데이터 불변성 - 참조 데이터

--- test/main.js
+++ test/main.js
@@ -1,17 +1,15 @@
-// 데이터 불변성(Immutability)
+// 참조형 데이터 : Object, Array, Function
+// 새로운 데이터를 만들 때마다 새로운 메모리 주소에 할당된다.(생긴 게 같아도 바라보는 곳이 다르다 = 가변성)
 
-// 원시 데이터 : string,number, boolean, undefined, null
-// 원시데이터는 메모리에 한번 올라가있으면 같은 데이터를 할당했을 때 새로운 메모리 주소로 지정하는게 아니라
-// 먼저 지정된 메모리 주소를 다시 참조한다 ( = 같은 곳을 다시 바라본다 = 불변성)
-
-let a = 1
-let b = 4
-console.log(a,b,a === b)
-b = a
-console.log(a,b,a === b)
-a = 7
-console.log(a,b,a === b)
-c = 1
-console.log(b,c,b === c)
-
-
+let a = { k : 1}
+let b = { k : 1}
+console.log(a,b,a === b) // {k: 1} , {k: 1} , false
+a.k = 7
+b = a // 같은 주소를 참조
+console.log(a,b, a === b) // {k: 7} , {k: 7} , true
+a.k = 2
+console.log(a, b, a === b) // {k: 2} , {k: 2} , true
+let c = b
+console.log(a, b, c, a === c) // {k: 2} , {k: 2} , true
+a.k = 9
+console.log(a,b,c, a === c) // {k: 9}, {k: 9} , true
