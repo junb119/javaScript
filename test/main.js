c4a9f907b805aab5b0661ce0577c56838d21acef
junb119 <emfowkd@gmail.com>
2022-08-12 21:09:53 +0900

    구조 분해 할당

--- test/main.js
+++ test/main.js
@@ -1,25 +1,25 @@
-// keys 정적 메소드 : Object 데이터의 key값으로 배열을 만드는 메소드
-// value 정적 메소드 : Object 데이터의 value값으로 배열을 만드는 메소드
-
+// 구조 분해 할당(비구조화 할당)
 
 const user = {
-  name : 'Heropy',
-  age : 85,
-  email : 'thesecond@gmail.com'
+  name: 'heropy',
+  age : 65,
+  email :'emfowkd@naver.com'
+  // address :'usa'
 }
 
-
-// key값 반환
-const keys = Object.keys(user)
-console.log(keys)
-// ['name', 'age', 'email']
-
-/* Object의 property에 접근하는 방법
- 1. user.email  
- 2. user['email']
- */
+// user의 원소를 각각의 변수에 할당가능.
+// 해당 원소가 없을시 기본값 지정 가능
+const { name :heropy , age, email, address = 'korea' } = user
+// name 이란 property를 heropy로 바꿔서 사용가능
+// E.g, user.address
 
 
-// value값 반환
-let values = keys.map(key => user[key])
-console.log(values)  // >>> ['Heropy', 85, 'thesecond@gmail.com']
+console.log(`사용자의 이름은 ${heropy} 입니다.`)
+console.log(`${name}의 나이는 ${age}세 입니다.`)
+console.log(`${name}의 이메일 주소는 ${user.email}입니다.`)
+console.log(address)
+
+// 비 구조화 할당
+const fruits = ['Apple', 'Banana', 'Cherry']
+const [, , b] = fruits
+console.log(b) // 'Cherry'
