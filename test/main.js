2a65b66b3ea374588cfcb36626f2db5909eab7bc
junb119 <emfowkd@gmail.com>
2022-08-12 22:23:09 +0900

    깊은 복사(lodash)

--- test/main.js
+++ test/main.js
@@ -1,21 +1,21 @@
-// 얕은 복사
+import _ from 'lodash'
+
+// 깊은 복사
 const user = {
   name: 'heropy',
   age: 85,
   emails: ['emfowkd@gmail.com']
 }
 
-
-// const copyUser = Object.assingn({}, user ) // assign 정적메소드를 이용한 얕은 복사
-const copyUser = {...user} // 전개 연산자를 이용한 얕은 복사
+const copyUser = _.cloneDeep(user) // lodash를 이용한 깊은 복사
 console.log(copyUser === user)
 
-user.age == 22
+user.age = 22
 console.log('user', user) // age : 22
 console.log('copyUser', copyUser) // age: 85
 
-user.emails.push('neo@zill.com') // user.emails = 배열데이터 = 참조형 데이터 = 같은 주소 참조
-console.log(user.emails === copyUser.emails) // true
+user.emails.push('neo@zill.com')
+console.log(user.emails === copyUser.emails) // false
 console.log('user', user)
 console.log('copyUser', copyUser) 
 
