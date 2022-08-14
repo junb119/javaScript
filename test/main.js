6364f77602cfce4e300d015d44492649ead70d5b
junb119 <emfowkd@gmail.com>
2022-08-14 15:26:57 +0900

    omdapi를 통해 영화정보 가져오기

--- test/main.js
+++ test/main.js
@@ -1,38 +1,17 @@
-const user = {
-  name: 'heropy',
-  age : 85,
-  emails :[
-    'thesecon@gmail.com',
-    'neo@zillinks.com'
-  ]
+
+// javascript
+import axios from 'axios'
+
+function fetchMovies()  {
+  axios
+    .get('https://www.omdbapi.com/?apikey=7035c60c&s=frozen') 
+    .then(res => {
+      const h1El = document.querySelector('h1')
+      const imgEl = document.querySelector('img')
+      h1El.textContent = res.data.Search[0].Title // h1태그에 영화제목 할당
+      imgEl.src = res.data.Search[0].Poster // img태그에 영화이미지 할당
+    })  
+  
+  
 }
-
-// localStorage.setItem('user', JSON.stringify(user) ) // 저장하기
-// console.log(JSON.parse(localStorage.getItem('user'))) // 가져오기
-// localStorage.removeItem('user') // 제거하기
-
-// user의 데이터를 json으로 문자데이터화해서 localstorage에 key, value값으로 지정
-
-
-
-
-console.log(JSON.parse(localStorage.getItem('user')))
-/*
-localstorage의 데이터를 받아와서 json.parse를 통해 객체화 시킨다음 콘솔에서 출력
-*/
-
-
-
-
-
-// localstorage의 데이터를 받아서 내용을 수정한다음 
-// 다시 localstorage에 반영하기
-
-const str = localStorage.getItem('user')
-const obj = JSON.parse(str)
-obj.age = 22
-console.log(obj)
-localStorage.setItem('user', JSON.stringify(obj))\
-
-// 쉽게 다루기 위한 패키지
-// lowdb
+  fetchMovies()
